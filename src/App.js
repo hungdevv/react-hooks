5fe9c76356aa19223613fe7be18f94654ee20791
hungdevv <hunghung0309@gmail.com>
2020-04-05 15:47:04 +0700

    todolist

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.scss";
-import ColorBox from "./components/ColorBox";
+import TodoList from "./components/TodoList";
+// import ColorBox from "./components/ColorBox";
 
 function App() {
+  const [todoList, setTodoList] = useState([
+    { id: 1, title: "List1" },
+    { id: 2, title: "List2" },
+    { id: 3, title: "List3" },
+    { id: 4, title: "List4" },
+  ]);
+
+  function handleTodoClick(todo) {
+    const index = todoList.findIndex((x) => x.id === todo.id);
+    if (index < 0) return;
+    const newTodoList = [...todoList];
+    newTodoList.splice(index, 1);
+    setTodoList(newTodoList);
+  }
+
   return (
     <div className="app">
-      <ColorBox />
+      <TodoList todos={todoList} onTodoClick={handleTodoClick} />
     </div>
   );
 }
